f680291dd0ebac00012d78c391cd11f920a0ac75
stivcs <stiven.castro@utp.edu.co>
2025-06-21 18:10:42 -0500

    reduccion de imagenes

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,6 +1,7 @@
 import { CommonModule } from '@angular/common';
 import { Component, ElementRef, OnInit, ViewChild } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import { ServiciosService } from './services/servicios.service';
 
 @Component({
   selector: 'app-root',
@@ -9,32 +10,19 @@ import { RouterOutlet } from '@angular/router';
   styleUrl: './app.component.css'
 })
 export class AppComponent implements OnInit {
-  imageList = [
-    'url("fondos/FONDOS/1.jpg")',
-    'url("fondos/FONDOS/2.jpg")',
-    'url("fondos/FONDOS/3.jpg")',
-    'url("fondos/FONDOS/4.jpg")',
-    'url("fondos/FONDOS/5.jpg")',
-    'url("fondos/FONDOS/6.jpg")',
-    'url("fondos/FONDOS/7.jpg")',
-    'url("fondos/FONDOS/8.jpg")',
-    'url("fondos/FONDOS/9.jpg")',
-    'url("fondos/FONDOS/10.jpg")',
-    'url("fondos/FONDOS/11.jpg")',
-    'url("fondos/FONDOS/12.jpg")',
-    'url("fondos/FONDOS/13.jpg")',
-    'url("fondos/FONDOS/14.jpg")',
-    'url("fondos/FONDOS/15.jpg")'
-  ];
+  constructor(private serviciosService: ServiciosService) { }
+  imageList: string[] = [];
   title = 'portafolio-juan';
   animateLogo = false;
   showSplash = true;
   currentBackground = this.imageList[0];
   currentIndex = 0;
+  esta: boolean = false;
   @ViewChild('splashLogo') splashLogo!: ElementRef<HTMLDivElement>;
   @ViewChild('logoTarget') logoTarget!: ElementRef<HTMLDivElement>;
 
   ngOnInit() {
+    this.imageList = this.serviciosService.getFondos();
     setTimeout(() => {
       this.animateLogoToHeader();
 
@@ -53,15 +41,28 @@ export class AppComponent implements OnInit {
     const splash = this.splashLogo.nativeElement;
     const target = this.logoTarget.nativeElement;
 
+    // CENTRAR splash manualmente en pantalla (sin usar CSS transform)
+    const viewportWidth = window.innerWidth;
+    const viewportHeight = window.innerHeight;
+
+    const splashWidth = splash.offsetWidth;
+    const splashHeight = splash.offsetHeight;
+
+    splash.style.left = `${(viewportWidth - splashWidth) / 2}px`;
+    splash.style.top = `${(viewportHeight - splashHeight) / 2}px`;
+    this.esta = true;
+
+    // Esperamos un pequeño momento para que se aplique la posición
+
     const splashRect = splash.getBoundingClientRect();
     const targetRect = target.getBoundingClientRect();
 
     const deltaX = targetRect.left + targetRect.width / 2 - (splashRect.left + splashRect.width / 2);
     const deltaY = targetRect.top + targetRect.height / 2 - (splashRect.top + splashRect.height / 2);
-
     const scale = targetRect.height / splashRect.height;
 
     splash.style.transition = 'transform 1s ease-in-out';
     splash.style.transform = `translate(${deltaX}px, ${deltaY}px) scale(${scale})`;
+
   }
 }
--- src/app/services/servicios.service.ts
+++ src/app/services/servicios.service.ts
@@ -6,4 +6,25 @@ import { Injectable } from '@angular/core';
 export class ServiciosService {
 
   constructor() { }
+
+  getFondos(): string[] {
+    return [
+      'url("fondos/1.webp")',
+      'url("fondos/2.webp")',
+      'url("fondos/3.webp")',
+      'url("fondos/4.webp")',
+      'url("fondos/5.webp")',
+      'url("fondos/6.webp")',
+      'url("fondos/7.webp")',
+      'url("fondos/8.webp")',
+      'url("fondos/9.webp")',
+      'url("fondos/10.webp")',
+      'url("fondos/11.webp")',  
+      'url("fondos/12.webp")',
+      'url("fondos/13.webp")',
+      'url("fondos/14.webp")',
+      'url("fondos/15.webp")',
+      'url("fondos/16.webp")'
+    ];
+  }
 }
